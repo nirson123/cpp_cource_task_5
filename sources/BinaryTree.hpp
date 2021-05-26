1bb01627381fcd7fb7c4ff5eef8930ae2426680c
nir_son <nir16832@gmail.com>
2021-05-26 19:36:33 +0300

    added notes

--- sources/BinaryTree.hpp
+++ sources/BinaryTree.hpp
@@ -10,6 +10,7 @@
 
 namespace ariel {
 
+    // node of a binary tree
     template<typename T>
     struct Node {
         T value;
@@ -23,15 +24,16 @@ namespace ariel {
     class BinaryTree {
 
     private:
-        Node<T>* root;
+        Node<T>* root;      // root of the tree
 
+        // an iterator for pre-order traversal of the tree
         class preorder_iterator {
         private:
-            std::vector<Node<T>*> vec;
+            std::vector<Node<T>*> vec;          // keep a vector of pointers to the nodes
             typename std::vector<Node<T>*>::iterator it;
             BinaryTree<T>& bt;
 
-            void traverse(Node<T> *current_node) {
+            void traverse(Node<T> *current_node) {  // fill the vector
                 if (current_node == nullptr) { return; }
                 vec.push_back(current_node);
                 traverse(current_node->left);
@@ -41,9 +43,9 @@ namespace ariel {
         public:
             explicit preorder_iterator(BinaryTree<T>& bt, bool end = false) : bt(bt){
                 traverse(bt.root);
-                vec.push_back(nullptr);
+                vec.push_back(nullptr); // to indicate the end
                 if (!end) { it = vec.begin(); }
-                else { it = --vec.end(); }
+                else { it = --vec.end(); }  // move to the nullptr
             }
 
             // iterator operators
@@ -55,13 +57,14 @@ namespace ariel {
             bool operator!=(const preorder_iterator &other) const { return !(*this == other); }
         }; // end of preorder iterator
 
+        // an iterator for in-order traversal of the tree
         class inorder_iterator {
         private:
-            std::vector<Node<T>*> vec;
+            std::vector<Node<T>*> vec;  // keep a vector of pointers to the nodes
             typename std::vector<Node<T>*>::iterator it;
             BinaryTree<T>& bt;
 
-            void traverse(Node<T>* current_node) {
+            void traverse(Node<T>* current_node) { // fill the vector
                 if (current_node == nullptr) { return; }
                 traverse(current_node->left);
                 vec.push_back(current_node);
@@ -71,9 +74,9 @@ namespace ariel {
         public:
             explicit inorder_iterator(BinaryTree<T>& bt, bool end = false) : bt(bt){
                 traverse(bt.root);
-                vec.push_back(nullptr);
+                vec.push_back(nullptr); // to indicate the end
                 if (!end) { it = vec.begin(); }
-                else { it = --vec.end(); }
+                else { it = --vec.end(); }    // move to the nullptr
             }
 
             // iterator operators
@@ -88,13 +91,14 @@ namespace ariel {
             friend BinaryTree<T>& BinaryTree<T>::add_right(const T &father, T val);
         };// end of inorder iterator
 
+        // an iterator for post-order traversal of the tree
         class postorder_iterator {
         private:
-            std::vector<Node<T>*> vec;
+            std::vector<Node<T>*> vec;  // keep a vector of pointers to the nodes
             typename std::vector<Node<T>*>::iterator it;
             BinaryTree<T>& bt;
 
-            void traverse(Node<T>* current_node) {
+            void traverse(Node<T>* current_node) {  // fill the vector
                 if (current_node == nullptr) { return; }
                 traverse(current_node->left);
                 traverse(current_node->right);
@@ -104,9 +108,9 @@ namespace ariel {
         public:
             explicit postorder_iterator(BinaryTree<T>& bt, bool end = false) : bt(bt){
                 traverse(bt.root);
-                vec.push_back(nullptr);
+                vec.push_back(nullptr); // to indicate the end
                 if (!end) { it = vec.begin(); }
-                else { it = --vec.end(); }
+                else { it = --vec.end(); }  // move to the nullptr
             }
 
             // iterator operators
@@ -118,6 +122,7 @@ namespace ariel {
             bool operator!=(const postorder_iterator &other) const { return !(*this == other); }
         };// end of postorder iterator
 
+        // release all the node's pointers
         void releaseNodes(Node<T>* current_node) {
             if (current_node == nullptr) { return; }
             releaseNodes(current_node->left);
@@ -126,15 +131,17 @@ namespace ariel {
         }
 
     public:
-        BinaryTree() : root(nullptr) {}
+        BinaryTree() : root(nullptr) {} // simple constructor
         ~BinaryTree() { releaseNodes(root); }
 
-        // add to the tree
+        // add (or change the data) root to the tree
         BinaryTree& add_root(T val) {
             if (root == nullptr) { root = new Node<T>(val); }
             else { root->value = val; }
             return *this;
         }
+
+        //add or change nodes in the graph (out-line implementation)
         BinaryTree& add_right(const T &father, T val);
         BinaryTree& add_left(const T &father, T val);
 
@@ -158,25 +165,32 @@ namespace ariel {
 
     template<typename T>
     BinaryTree<T>& BinaryTree<T>::add_right(const T &father, T val) {
+        // find the father node
         auto it1 = begin_inorder();
         while (it1 != end_inorder() && *it1 != father) { ++it1; }
+
+        // if not found, throw exception
         if (it1 == end_inorder()) { throw std::invalid_argument("the value not found in the tree!"); }
 
-        Node<T>* fatherNode = *(it1.it);
-        if (fatherNode->right == nullptr) { fatherNode->right = new Node<T>(val); }
-        else { fatherNode->right->value = val; }
+
+        Node<T>* fatherNode = *(it1.it);    // get the node object
+        if (fatherNode->right == nullptr) { fatherNode->right = new Node<T>(val); } // add
+        else { fatherNode->right->value = val; }    // change
         return *this;
     }
 
     template<typename T>
     BinaryTree<T>& BinaryTree<T>::add_left(const T &father, T val) {
+        // find the father node
         auto it1 = begin_inorder();
         while (it1 != end_inorder() && *it1 != father) { ++it1; }
+
+        // if not found, throw exception
         if (it1 == end_inorder()) { throw std::invalid_argument("the value not found in the tree!"); }
 
-        Node<T>* fatherNode = *(it1.it);
-        if (fatherNode->left == nullptr) { fatherNode->left = new Node<T>(val); }
-        else { fatherNode->left->value = val; }
+        Node<T>* fatherNode = *(it1.it);    // get the node object
+        if (fatherNode->left == nullptr) { fatherNode->left = new Node<T>(val); } // add
+        else { fatherNode->left->value = val; } // change
         return *this;
     }
 
@@ -184,14 +198,20 @@ namespace ariel {
     std::ostream& operator<<(std::ostream& os, const BinaryTree<T>& bt) {return print_tree(os, bt.root, 0);}
 
     template<typename T>
-    std::ostream& print_tree(std::ostream &os, const Node<T>* current_node, int space) {
+    std::ostream& print_tree(std::ostream& os, const Node<T>* current_node, int space) {
+        static const int SPACES = 10;   // difference between lines
+
         if(current_node == nullptr){return os;}
-        space += 10;
-        print_tree(os, current_node->right, space);
+
+        space += SPACES;
+        print_tree(os, current_node->right, space); // recursively print right child
         os << std::endl;
-        for(int i = 10; i < space; i++){os << " ";}
-        os << current_node->value << std::endl;
-        print_tree(os, current_node->left, space);
+
+        for(int i = SPACES; i < space; i++){os << " ";} // print spaces
+
+        os << current_node->value << std::endl; // print value
+
+        print_tree(os, current_node->left, space);  // recursively print left child
         return os;
     }
 }
