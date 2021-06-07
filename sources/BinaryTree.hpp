9849e0d19eaf76b50248a4b3c96d17ba277ed8a3
nir_son <nir16832@gmail.com>
2021-06-07 20:23:54 +0300

    part b

--- sources/BinaryTree.hpp
+++ sources/BinaryTree.hpp
@@ -7,6 +7,8 @@
 
 #include <vector>
 #include <string>
+#include <exception>
+#include <iostream>
 
 namespace ariel {
 
@@ -31,7 +33,7 @@ namespace ariel {
         private:
             std::vector<Node<T>*> vec;          // keep a vector of pointers to the nodes
             typename std::vector<Node<T>*>::iterator it;
-            BinaryTree<T>& bt;
+            Node<T>* bt;
 
             void traverse(Node<T> *current_node) {  // fill the vector
                 if (current_node == nullptr) { return; }
@@ -41,18 +43,53 @@ namespace ariel {
             }
 
         public:
-            explicit preorder_iterator(BinaryTree<T>& bt, bool end = false) : bt(bt){
-                traverse(bt.root);
+            explicit preorder_iterator(BinaryTree<T>& bt, bool end = false) : bt(bt.root){
+                traverse(this->bt);
                 vec.push_back(nullptr); // to indicate the end
                 if (!end) { it = vec.begin(); }
                 else { it = --vec.end(); }  // move to the nullptr
             }
 
+            preorder_iterator(const preorder_iterator& other) : vec(other.vec), bt(other.bt){
+                this->it = this->vec.begin();
+                int n_moves = other.it - other.vec.begin();
+                for(int i = 0; i < n_moves; i++){++(this->it);}
+            }
+
+            preorder_iterator& operator=(const preorder_iterator& other){
+                if(this == &other){return *this;}
+                this->vec = other.vec;
+                this->bt = other.bt;
+                this->it = this->vec.begin();
+                int n_moves = other.it - other.vec.begin();
+                for(int i = 0; i < n_moves; i++){++(this->it);}
+                return *this;
+            }
+
+            preorder_iterator(const preorder_iterator&& other)  noexcept : vec(other.vec), bt(other.bt){
+                this->it = this->vec.begin();
+                int n_moves = other.it - other.vec.begin();
+                for(int i = 0; i < n_moves; i++){++(this->it);}
+            }
+
+            preorder_iterator& operator=(preorder_iterator&& other) noexcept {
+                this->vec = other.vec;
+                this->bt = other.bt;
+                this->it = this->vec.begin();
+                int n_moves = other.it - other.vec.begin();
+                for(int i = 0; i < n_moves; i++){++(this->it);}
+                return *this;
+            }
+
+            ~preorder_iterator() = default;
+
+
+
             // iterator operators
             T &operator*() const { return (*it)->value; }
             T *operator->() const { return &((*it)->value); }
             preorder_iterator &operator++() {++it; return *this;}
-            const preorder_iterator operator++(int) {preorder_iterator temp = *this; it++; return temp;}
+            preorder_iterator operator++(int) {preorder_iterator temp = *this; it++; return temp;}
             bool operator==(const preorder_iterator &other) const { return (*(this->it) == *(other.it)); }
             bool operator!=(const preorder_iterator &other) const { return !(*this == other); }
         }; // end of preorder iterator
@@ -62,7 +99,7 @@ namespace ariel {
         private:
             std::vector<Node<T>*> vec;  // keep a vector of pointers to the nodes
             typename std::vector<Node<T>*>::iterator it;
-            BinaryTree<T>& bt;
+            Node<T>* bt;
 
             void traverse(Node<T>* current_node) { // fill the vector
                 if (current_node == nullptr) { return; }
@@ -72,18 +109,52 @@ namespace ariel {
             }
 
         public:
-            explicit inorder_iterator(BinaryTree<T>& bt, bool end = false) : bt(bt){
-                traverse(bt.root);
+            explicit inorder_iterator(BinaryTree<T>& bt, bool end = false) : bt(bt.root){
+                traverse(this->bt);
                 vec.push_back(nullptr); // to indicate the end
                 if (!end) { it = vec.begin(); }
                 else { it = --vec.end(); }    // move to the nullptr
             }
 
+
+            inorder_iterator(const inorder_iterator& other) : vec(other.vec), bt(other.bt){
+                this->it = this->vec.begin();
+                int n_moves = other.it - other.vec.begin();
+                for(int i = 0; i < n_moves; i++){++(this->it);}
+            }
+
+            inorder_iterator& operator=(const inorder_iterator& other){
+                if(this == &other){return *this;}
+                this->vec = other.vec;
+                this->bt = other.bt;
+                this->it = this->vec.begin();
+                int n_moves = other.it - other.vec.begin();
+                for(int i = 0; i < n_moves; i++){++(this->it);}
+                return *this;
+            }
+
+            inorder_iterator(const inorder_iterator&& other)  noexcept : vec(other.vec), bt(other.bt){
+                this->it = this->vec.begin();
+                int n_moves = other.it - other.vec.begin();
+                for(int i = 0; i < n_moves; i++){++(this->it);}
+            }
+
+            inorder_iterator& operator=(inorder_iterator&& other) noexcept {
+                this->vec = other.vec;
+                this->bt = other.bt;
+                this->it = this->vec.begin();
+                int n_moves = other.it - other.vec.begin();
+                for(int i = 0; i < n_moves; i++){++(this->it);}
+                return *this;
+            }
+
+            ~inorder_iterator() = default;
+
             // iterator operators
             T &operator*() const { return (*it)->value; }
             T *operator->() const { return &((*it)->value); }
             inorder_iterator &operator++() {++it; return *this;}
-            const inorder_iterator operator++(int) {inorder_iterator temp = *this; it++; return temp;}
+            inorder_iterator operator++(int) {inorder_iterator temp = *this; it++; return temp;}
             bool operator==(const inorder_iterator &other) const { return (*(this->it) == *(other.it)); }
             bool operator!=(const inorder_iterator &other) const { return !(*this == other); }
 
@@ -96,7 +167,7 @@ namespace ariel {
         private:
             std::vector<Node<T>*> vec;  // keep a vector of pointers to the nodes
             typename std::vector<Node<T>*>::iterator it;
-            BinaryTree<T>& bt;
+            Node<T>* bt;
 
             void traverse(Node<T>* current_node) {  // fill the vector
                 if (current_node == nullptr) { return; }
@@ -106,18 +177,53 @@ namespace ariel {
             }
 
         public:
-            explicit postorder_iterator(BinaryTree<T>& bt, bool end = false) : bt(bt){
-                traverse(bt.root);
+            explicit postorder_iterator(BinaryTree<T>& bt, bool end = false) : bt(bt.root){
+                traverse(this->bt);
                 vec.push_back(nullptr); // to indicate the end
                 if (!end) { it = vec.begin(); }
                 else { it = --vec.end(); }  // move to the nullptr
             }
 
+
+            postorder_iterator(const postorder_iterator& other) : vec(other.vec), bt(other.bt){
+                this->it = this->vec.begin();
+                int n_moves = other.it - other.vec.begin();
+                for(int i = 0; i < n_moves; i++){++(this->it);}
+            }
+
+            postorder_iterator& operator=(const postorder_iterator& other){
+                if(this == &other){return *this;}
+                this->vec = other.vec;
+                this->bt = other.bt;
+                this->it = this->vec.begin();
+                int n_moves = other.it - other.vec.begin();
+                for(int i = 0; i < n_moves; i++){++(this->it);}
+                return *this;
+            }
+
+            postorder_iterator(const postorder_iterator&& other)  noexcept : vec(other.vec), bt(other.bt){
+                this->it = this->vec.begin();
+                int n_moves = other.it - other.vec.begin();
+                for(int i = 0; i < n_moves; i++){++(this->it);}
+            }
+
+            postorder_iterator& operator=(postorder_iterator&& other) noexcept {
+                this->vec = other.vec;
+                this->bt = other.bt;
+                this->it = this->vec.begin();
+                int n_moves = other.it - other.vec.begin();
+                for(int i = 0; i < n_moves; i++){++(this->it);}
+                return *this;
+            }
+
+            ~postorder_iterator() = default;
+
+
             // iterator operators
             T &operator*() const { return (*it)->value; }
             T *operator->() const { return &((*it)->value); }
             postorder_iterator &operator++() {++it; return *this;}
-            const postorder_iterator operator++(int) {postorder_iterator temp = *this; it++; return temp;}
+            postorder_iterator operator++(int) {postorder_iterator temp = *this; it++; return temp;}
             bool operator==(const postorder_iterator &other) const { return (*(this->it) == *(other.it)); }
             bool operator!=(const postorder_iterator &other) const { return !(*this == other); }
         };// end of postorder iterator
@@ -130,9 +236,32 @@ namespace ariel {
             delete current_node;
         }
 
+        void copy_by_value(const Node<T>* other, Node<T>*& in_this){
+            in_this = new Node<T>(other->value);
+            if(other->left != nullptr){copy_by_value(other->left, in_this->left);}
+            if(other->right != nullptr){copy_by_value(other->right, in_this->right);}
+        }
+
     public:
         BinaryTree() : root(nullptr) {} // simple constructor
+        BinaryTree(const BinaryTree<T>& other){copy_by_value(other.root, this->root);}    // copy constructor
+        BinaryTree(BinaryTree<T>&& other)  noexcept : root(other.root){other.root = nullptr;}      // moving constructor
         ~BinaryTree() { releaseNodes(root); }
+        
+        // assignment operator
+        BinaryTree<T>& operator=(const BinaryTree<T>& other){
+            if(this == &other){return *this;}
+            releaseNodes(this->root);
+            copy_by_value(other.root, this->root);
+            return *this;
+        }
+
+        // moving operator
+        BinaryTree<T>& operator=(BinaryTree<T>&& other) noexcept {
+            releaseNodes(this->root);
+            this->root = other.root;
+            other.root = nullptr;
+        }
 
         // add (or change the data) root to the tree
         BinaryTree& add_root(T val) {
@@ -167,7 +296,7 @@ namespace ariel {
     BinaryTree<T>& BinaryTree<T>::add_right(const T &father, T val) {
         // find the father node
         auto it1 = begin_inorder();
-        while (it1 != end_inorder() && *it1 != father) { ++it1; }
+        while (it1 != end_inorder() && !(*it1 == father)) { ++it1; }
 
         // if not found, throw exception
         if (it1 == end_inorder()) { throw std::invalid_argument("the value not found in the tree!"); }
@@ -183,7 +312,7 @@ namespace ariel {
     BinaryTree<T>& BinaryTree<T>::add_left(const T &father, T val) {
         // find the father node
         auto it1 = begin_inorder();
-        while (it1 != end_inorder() && *it1 != father) { ++it1; }
+        while (it1 != end_inorder() && !(*it1 == father)) { ++it1; }
 
         // if not found, throw exception
         if (it1 == end_inorder()) { throw std::invalid_argument("the value not found in the tree!"); }
